a3d700fc3f3e6639e3b59f71eae5b8e5932610fb
Jhio0 <120062814+Jhio0@users.noreply.github.com>
2023-11-10 12:21:34 -0700

    finish the mockupdesign for the hompage

---
+++ EcommerceWebsite/my-app/app/components/categoryList.js
@@ -0,0 +1,41 @@
+export const categoryList = [
+    {
+        name: "PowerTools",
+        id: 1,
+        img: 'https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2607&q=80'
+    },
+    {
+        name: "HandTools",
+        id: 2,
+        img: 'https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2607&q=80'
+    },
+    {
+        name: "GardeningTools",
+        id: 3,
+        img: 'https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2607&q=80'
+    },
+    {
+        name: "ConstructionTools",
+        id: 4,
+        img: 'https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2607&q=80'
+    },
+    {
+        name: "PowerTools",
+        id: 5,
+        img: 'https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2607&q=80'
+    },
+    {
+        name: "AutomotiveTools",
+        id: 6,
+        img: 'https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2607&q=80'
+    },
+    {
+        id: 7,
+        img: 'https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2607&q=80'
+    },
+    {
+        id: 8,
+        img: 'https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2607&q=80'
+    },
+
+];
---
+++ EcommerceWebsite/my-app/app/components/header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {AiOutlineHeart, AiOutlineShoppingCart} from 'react-icons/ai'
+
+const Box = () => {
+  return (
+    <div className="flex-center ">
+        <div className="absolute w-[438px] h-[24px] top-[35px] left-[472px]">
+            <div className="left-0 absolute top-0 [font-family:'Poppins-Medium',Helvetica] font-medium text-black text-[16px] tracking-[0] leading-[normal]">
+            Home
+            </div>
+            <div className="left-[123px] absolute top-0 [font-family:'Poppins-Medium',Helvetica] font-medium text-black text-[16px] tracking-[0] leading-[normal]">
+            Shop
+            </div>
+            <div className="left-[240px] absolute top-0 [font-family:'Poppins-Medium',Helvetica] font-medium text-black text-[16px] tracking-[0] leading-[normal]">
+            About
+            </div>
+            <div className="absolute top-0 left-[364px] [font-family:'Poppins-Medium',Helvetica] font-medium text-black text-[16px] tracking-[0] leading-[normal]">
+            Contact
+            </div>
+        </div>
+        <div className="relative top-[20px]">
+            <div><AiOutlineHeart className="text-black absolute w-[28px] h-[23px] top-[9px] left-[1250px]" /></div>
+            <div><AiOutlineShoppingCart className="text-black absolute w-[28px] h-[23px] top-[9px] left-[1310px]" /></div>
+            <div className="inline-flex items-center justify-center gap-[5px] absolute top-0 left-0">
+            <img className="relative w-[50px] h-[32px]" alt="Meubel house logos" src="/favicon.ico" />
+            <div className="relative w-fit mt-[-1.00px] [font-family:'Montserrat-Bold',Helvetica] font-bold text-black text-[34px] tracking-[0] leading-[normal]">
+                Rent-Tool
+            </div>
+            </div>
+        </div>
+    </div>
+    
+  );
+};
+export {Box};
